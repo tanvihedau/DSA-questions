759d1dc5542b21bc902911fff35b4cc86d52ac37
Tanvi Hedau <tanvihedau28@gmail.com>
2025-05-12 13:50:56 +0530

    Count Pairs whose sum is less than target.cpp

---
+++ Count Pairs whose sum is less than target.cpp
@@ -0,0 +1,6 @@
+int countPairs(vector<int> &arr, int target) {
+        sort(arr.begin(), arr.end());
+        int l = 0, r = arr.size()-1, ans= 0;
+        while(l<r) (arr[l]+ arr[r]<target)?ans+=(r-l), l++:r--;
+        return ans;
+    }
